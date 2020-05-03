d0ff4c8004518a66e296c36746e2bce0b2062a04
Wright-Dj <wrightdj2000@gmail.com>
2020-05-02 20:08:16 -0700

    Update MRCPMeter.h

--- src/MRCPMeter.h
+++ src/MRCPMeter.h
@@ -81,92 +81,91 @@ class MRCPMeter : public MRCPNode{
         if(client_authenticated){
           distance = readRange();
 
-        if(distance > 40) // Used for Testing
-          digitalWrite(LED, HIGH);
-        else
-          digitalWrite(LED, LOW);
-        Serial.print("Distance: ");
-        Serial.println(distance);
+          if(distance > 40) // Used for Testing
+            digitalWrite(LED, HIGH);
+          else
+            digitalWrite(LED, LOW);
+          Serial.print("Distance: ");
+          Serial.println(distance);
 
 
-        if(state == 3) {
-                 // State: s3 = Valid Parking
-                 distance = readRange();
+          if(state == 3) {
+                  // State: s3 = Valid Parking
+                  distance = readRange();
 
-                 if(distance < minDetectionDistance){
-                   // State: s4 = Maintance Required (VALID), LED = Solid Green
-                   // If sensor gets obstructed, then stay here until resolved.
-                   digitalWrite(ledGreen, HIGH);
-                   distance = readRange();
-                 }
-                 else if(distance>(sonicSensor_init+tolerance))
-                 {
-                   duration = difftime( time(0), start);  // Stopping the clock
-                   Serial.println("Stopping!");
-                   Serial.println(duration);
-                   double totalCost = round(duration * (rate));  // Total cost to customer
-                   // process payment?
-                   Serial.println(totalCost);
-                   tx_doc["type"] = "STOP";
-                   sendDoc(tx_doc);
-                   state = 0;
-                 }else{
-                  if(millis() % 1000 > 500){
-                    digitalWrite(ledGreen, LOW);
-                  }else{
+                  if(distance < minDetectionDistance){
+                    // State: s4 = Maintance Required (VALID), LED = Solid Green
+                    // If sensor gets obstructed, then stay here until resolved.
                     digitalWrite(ledGreen, HIGH);
+                    distance = readRange();
                   }
-                 }
-
-            }else if (distance < maxDetectionDistance) {
-              if(distance < minDetectionDistance){
-                // State: s5 = Maintance Required (Invalid Parking), LED = Solid Red
-                // If sensor gets obstructed, then stay here until resolved.
-                digitalWrite(ledRed, HIGH);
-                distance = readRange();
-                }
-              else{
-                digitalWrite(ledRed, LOW);
-
-                if(state == 0){
-                  initialDistance = distance; // Saving the distance the car is currently at
-                  start = time(0);     // Restarting the clock
-                }
-
-                if(distance <= (initialDistance+tolerance)){
-                  duration = difftime( time(0), start);
-                  if(duration >= violationTimeLimit){
-                    // State: s2 = violation, LED = Blink RED
-                    state = 2;
-                    if(millis() % 500 > 250){
-                     digitalWrite(ledRed, LOW);
+                  else if(distance>(sonicSensor_init+tolerance))
+                  {
+                    duration = difftime( time(0), start);  // Stopping the clock
+                    Serial.println("Stopping!");
+                    Serial.println(duration);
+                    double totalCost = round(duration * (rate));  // Total cost to customer
+                    // process payment?
+                    Serial.println(totalCost);
+                    tx_doc["type"] = "STOP";
+                    sendDoc(tx_doc);
+                    state = 0;
+                  }else{
+                    if(millis() % 1000 > 500){
+                      digitalWrite(ledGreen, LOW);
                     }else{
-                     digitalWrite(ledRed, HIGH);
-                    };
+                      digitalWrite(ledGreen, HIGH);
+                    }
+                  }
+
+              }else if (distance < maxDetectionDistance) {
+                if(distance < minDetectionDistance){
+                  // State: s5 = Maintance Required (Invalid Parking), LED = Solid Red
+                  // If sensor gets obstructed, then stay here until resolved.
+                  digitalWrite(ledRed, HIGH);
+                  distance = readRange();
                   }
                 else{
-                  // State: s1 = standby, LED = Blink Yellow
-                  state = 1;
-                  if(millis() % 1000 > 500){
-                    digitalWrite(ledGreen, HIGH);
-                    digitalWrite(ledBlue, HIGH);
-                    digitalWrite(ledRed, HIGH);
-                  }else{
-                    digitalWrite(ledGreen, LOW);
-                    digitalWrite(ledBlue, LOW);
-                    digitalWrite(ledRed, LOW);
+                  digitalWrite(ledRed, LOW);
+
+                  if(state == 0){
+                    initialDistance = distance; // Saving the distance the car is currently at
+                    start = time(0);     // Restarting the clock
+                  }
+
+                  if(distance <= (initialDistance+tolerance)){
+                    duration = difftime( time(0), start);
+                    if(duration >= violationTimeLimit){
+                      // State: s2 = violation, LED = Blink RED
+                      state = 2;
+                      if(millis() % 500 > 250){
+                      digitalWrite(ledRed, LOW);
+                      }else{
+                      digitalWrite(ledRed, HIGH);
+                      };
+                    }
+                  else{
+                    // State: s1 = standby, LED = Blink Yellow
+                    state = 1;
+                    if(millis() % 1000 > 500){
+                      digitalWrite(ledGreen, HIGH);
+                      digitalWrite(ledBlue, HIGH);
+                      digitalWrite(ledRed, HIGH);
+                    }else{
+                      digitalWrite(ledGreen, LOW);
+                      digitalWrite(ledBlue, LOW);
+                      digitalWrite(ledRed, LOW);
+                    }
                   }
                 }
+                distance = readRange();
               }
-              distance = readRange();
-            }
-        }
-        else{
-          state = 0;
+          }
+          else{
+            state = 0;
 
-        }
-        // else, State: s0 = Available, LED = off
-        delay(100);
+          }
+          // else, State: s0 = Available, LED = off
         }
       }
 };
